c20ffa3b5900568ad1d6f3edb80654914d1c4993
103604 <103604@glr.nl>
2026-03-20 16:04:55 +0100

    Quiz werkend prototype (deze keer werkt het wel)

---
+++ Home-pagina/quiz/Quiz-1.js
@@ -0,0 +1,39 @@
+let vragen = [
+    {
+        vraag: "Wat is 2 + 2?",
+        goed: "b"
+    },
+    {
+        vraag: "Wat is de hoofdstad van Nederland?",
+        goed: "a"
+    },
+    {
+        vraag: "Wat is 3 x 3?",
+        goed: "b"
+    }
+];
+
+let vraagAantal = 0;
+let punten = 0;
+
+window.addEventListener("load", function () {
+    document.getElementById("vraag").innerHTML = vragen[0].vraag;
+})
+
+function checkAntwoord(antwoord) {
+    const huidigeVraag = vragen[vraagAantal];
+    if (vragen.length === vraagAantal) {
+        alert("de quiz is klaar");
+        return;
+    }
+    if (huidigeVraag.goed === antwoord) {
+        punten++;
+        
+    }
+    else {
+        alert("Fout")
+    }
+    document.getElementById("score").innerHTML = punten;
+    vraagAantal++;
+    document.getElementById("vraag").innerHTML = vragen[vraagAantal].vraag;
+}
